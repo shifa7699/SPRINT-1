225bbc10513401d752da0092b22876182f0f4743
Shifa Iqbal <111556272+shifa7699@users.noreply.github.com>
2023-03-30 09:45:09 +0530

    Delete Project2 directory

--- Project2/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package Test.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Test.Entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>
-{
-}
